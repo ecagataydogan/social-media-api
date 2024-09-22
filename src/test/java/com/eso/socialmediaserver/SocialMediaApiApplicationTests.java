6be25d61ae73ded8cecb98b92937ddd9a245d78b
esref <ecagataydgn@gmail.com>
2024-09-22 22:22:09 +0300

    initial dependencies

---
+++ src/test/java/com/eso/socialmediaserver/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eso.socialmediaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
