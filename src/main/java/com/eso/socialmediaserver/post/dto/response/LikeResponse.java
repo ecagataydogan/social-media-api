01c411f9cfd951ffdbd1d78e83d27e732b86a61e
esref <ecagataydgn@gmail.com>
2024-09-26 23:43:31 +0300

    add like response

--- src/main/java/com/eso/socialmediaserver/post/dto/response/LikeResponse.java
+++ src/main/java/com/eso/socialmediaserver/post/dto/response/LikeResponse.java
@@ -1,5 +1,12 @@
 package com.eso.socialmediaserver.post.dto.response;
 
+import com.eso.socialmediaserver.client.dto.response.ClientResponse;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class LikeResponse {
-    // todo
+    private Long id;
+    private ClientResponse client;
 }
